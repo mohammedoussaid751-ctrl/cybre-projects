9fe4bc2bcac05be7307b88b2f296a21e4c9ea321
mohammed.osd <mohammedoussaid751@gmail.com>
2026-04-18 13:21:09 +0100

    Create port_scanner.py
    Simple Port Scanner
    This tool scans open ports on a target IP.
    Made for learning networking and cybersecurity basics.

---
+++ port_scanner.py
@@ -0,0 +1,19 @@
+import socket
+
+target = "127.0.0.1"
+
+print(f"Scanning target: {target}")
+
+for port in range(1, 100):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+
+    s.close()
+
+
+
+
